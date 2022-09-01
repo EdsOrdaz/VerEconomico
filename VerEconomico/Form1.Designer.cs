bc62aa81471bf4c6b73f02a0e6dc3d70692d28e4
Edson Ordaz <edsordaz@hotmail.com>
2022-09-01 09:13:30 -0500

    Agregar archivos de proyecto.

---
+++ VerEconomico/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace VerEconomico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dato1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dato2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Silver;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dato1,
+            this.dato2});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 102;
+            this.dataGridView1.Size = new System.Drawing.Size(414, 201);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // dato1
+            // 
+            this.dato1.HeaderText = "Column1";
+            this.dato1.Name = "dato1";
+            this.dato1.ReadOnly = true;
+            this.dato1.Width = 130;
+            // 
+            // dato2
+            // 
+            this.dato2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dato2.HeaderText = "Column1";
+            this.dato2.Name = "dato2";
+            this.dato2.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 201);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos del Equipo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dato1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dato2;
+    }
+}
+
---
+++ VerEconomico/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerEconomico
+{
+    public partial class Form1 : Form
+    {
+        private String versiontext = "11.1";
+        private String version = "1ce61e3ed2e368a4f295a36748276df98b06b331";
+        public static String conexionsqllast = "server=148.223.153.37,5314; database=InfEq;User ID=eordazs;Password=Corpame*2013; integrated security = false ; MultipleActiveResultSets=True";
+
+        public static String servivor = "148.223.153.43\\MSSQLSERVER1";
+        public static String basededatos = "bd_SiRAc";
+        public static String usuariobd = "sa";
+        public static String passbd = "At3n4";
+        public static string nsql = "server=" + servivor + "; database=" + basededatos + " ;User ID=" + usuariobd + ";Password=" + passbd + "; integrated security = false ; MultipleActiveResultSets=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string funcion_wmi(String database, String dato)
+        {
+            ConnectionOptions options = new ConnectionOptions();
+            options.Impersonation = System.Management.ImpersonationLevel.Impersonate;
+
+
+            ManagementScope scope = new ManagementScope("\\\\" + Environment.MachineName.ToString() + "\\root\\cimv2", options);
+            scope.Connect();
+
+            ObjectQuery query = new ObjectQuery("SELECT * FROM " + database);
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            String cadena = "";
+            ManagementObjectCollection queryCollection = searcher.Get();
+            foreach (ManagementObject m in queryCollection)
+            {
+                cadena = m[dato].ToString();
+            }
+            dato = null;
+            database = null;
+            if (cadena.Equals(null))
+            {
+                return "Error";
+            }
+            return cadena;
+        }
+
+        public static String NumSerie()
+        {
+            String[] data = new string[] {
+                   "Win32_BIOS",
+                    "SerialNumber",
+                    "Sin Resultado"
+                };
+            return (funcion_wmi(data[0], data[1]) == "") ? data[2] : funcion_wmi(data[0], data[1]);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //Validar Version
+            try
+            {
+                using (SqlConnection conexion2 = new SqlConnection(conexionsqllast))
+                {
+                    conexion2.Open();
+                    String sql2 = "SELECT (SELECT valor FROM Configuracion WHERE nombre='VerEconomico_Version') as version,valor FROM Configuracion WHERE nombre='VerEconomico_hash'";
+                    SqlCommand comm2 = new SqlCommand(sql2, conexion2);
+                    SqlDataReader nwReader2 = comm2.ExecuteReader();
+                    if (nwReader2.Read())
+                    {
+                        if (nwReader2["valor"].ToString() != version)
+                        {
+                            MessageBox.Show("No se puede inciar porque hay una nueva version.\n\nNueva Version: " + nwReader2["version"].ToString() + "\nVersion actual: " + versiontext + "\n\nEl programa se cerrara.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            Application.Exit();
+                            return;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en validar la version del programa\n\nMensaje: " + ex.Message, "Información del Equipo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+                return;
+            }
+
+
+
+            try
+            {
+                using (SqlConnection conexion2 = new SqlConnection(nsql))
+                {
+                    conexion2.Open();
+                    String sql2 = "SELECT * FROM [bd_SiRAc].[dbo].[LNJ_Equipos_Gilberto] where [No. Serie]='"+NumSerie()+"'";
+                    SqlCommand comm2 = new SqlCommand(sql2, conexion2);
+                    SqlDataReader nwReader2 = comm2.ExecuteReader();
+
+                    if (nwReader2.Read())
+                    {
+                        dataGridView1.Rows.Add("Número economico:", nwReader2["No. Activo"].ToString());
+                        dataGridView1.Rows.Add("Fecha de Alta:", nwReader2["Fecha SIRAC"].ToString());
+                        dataGridView1.Rows.Add("Marca:", nwReader2["marca"].ToString());
+                        dataGridView1.Rows.Add("Modelo:", nwReader2["modelo"].ToString());
+                        dataGridView1.Rows.Add("Número de Serie:", NumSerie());
+                        dataGridView1.Rows.Add("Resguardado a:", nwReader2["Nombre de Resguardatario"].ToString());
+                        dataGridView1.Rows.Add("Centro de costos:", nwReader2["Centro de Costo"].ToString());
+                        dataGridView1.Rows.Add("Base:", nwReader2["Base"].ToString());
+                        dataGridView1.Rows.Add("Fecha de Asignación:", nwReader2["Fecha Asignación"].ToString());
+
+                    }
+                    System.Windows.Forms.DataGridViewCellStyle boldStyle = new System.Windows.Forms.DataGridViewCellStyle();
+                    boldStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+
+                    for (int i = 0; i <= 8; i++)
+                    {
+                        dataGridView1.Rows[i].Cells[0].Style = boldStyle;
+                        dataGridView1.Rows[i].Cells[0].Style.BackColor = Color.Silver;
+                    }
+                    this.dataGridView1.ClearSelection();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en validar la version del programa\n\nMensaje: " + ex.Message, "Información del Equipo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+                return;
+            }
+        }
+    }
+}
